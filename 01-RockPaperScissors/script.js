acfe0f45b43c2a659a19ca217a1be280759e6440
Kieran Taylor <kmtaylor00@outlook.com>
2022-01-26 18:23:33 +0200

    complete game

--- 01-RockPaperScissors/script.js
+++ 01-RockPaperScissors/script.js
@@ -1,7 +1,7 @@
 "use strict";
-// alert(
-//   "You will be playing a best of 5 game of Rock, Paper and Scissors against the computer"
-// );
+alert(
+  "You will be playing a best of 5 game of Rock, Paper and Scissors against the computer, highest score wins!\nGood Luck!!:D"
+);
 
 const computerPlay = function () {
   const game = ["Rock", "Paper", "Scissors"];
@@ -9,28 +9,53 @@ const computerPlay = function () {
   return random;
 };
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   const player =
     playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
   const computer = computerSelection;
 
   if (player === "Rock" && computer === "Scissors") {
+    playerScore++;
     return `You Win! ${player} beats ${computer}`;
   } else if (computer === "Rock" && player === "Scissors") {
+    computerScore++;
     return `You lose! ${computer} beats ${player}`;
   } else if (player === "Paper" && computer === "Rock") {
+    playerScore++;
     return `You Win! ${player} beats ${computer}`;
   } else if (computer === "Paper" && player === "Rock") {
+    computerScore++;
     return `You lose! ${computer} beats ${player}`;
   } else if (player === "Scissors" && computer === "Paper") {
+    playerScore++;
     return `You Win! ${player} beats ${computer}`;
   } else if (computer === "Scissors" && player === "Paper") {
+    computerScore++;
     return `You lose! ${computer} beats ${player}`;
   } else {
-    return `It's a draw!`;
+    return `No points scored!`;
   }
 }
 
-function game() {}
+function game() {
+  let i = 1;
+  while (i < 6) {
+    const playerSelection = prompt(`Round ${[i++]}: \nRock, Paper, Scissors`);
+    const computerSelection = computerPlay();
+    alert(playRound(playerSelection, computerSelection));
+  }
+  alert(`Player Score = ${playerScore} \nComputer Score = ${computerScore}`);
 
-const computerSelection = computerPlay();
+  if (computerScore > playerScore) {
+    alert(`Computer Wins!`);
+  } else if (computerScore > playerScore) {
+    alert(`Player Wins!`);
+  } else {
+    alert(`It's a draw!`);
+  }
+}
+
+game();
