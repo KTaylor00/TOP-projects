94bbde0f4d97595ee8e852713ca522c505c385c2
Kieran Taylor <kmtaylor00@outlook.com>
2022-02-15 14:21:52 +0200

    complete etch-a-sketch project basic, will make it look better and add new features.

--- 02-etch-a-sketch/script.js
+++ 02-etch-a-sketch/script.js
@@ -1,19 +1,52 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const btnContainer = document.querySelector(".btn-container");
+const btnContainerGrid = document.querySelector(".btn-container-grid");
+
+const btn = document.createElement("button");
+btn.classList.add("reset-grid");
+btn.textContent = "Change grid size";
+btnContainerGrid.appendChild(btn);
+
+const btnReset = document.createElement("button");
+btnReset.classList.add("reset");
+btnReset.textContent = "Reset";
+btnContainer.appendChild(btnReset);
+
+function randomColour() {
+  let letters = "0123456789ABCDEF";
+  let colour = "#";
+  for (let i = 0; i < 6; i++) {
+    colour += letters[Math.floor(Math.random() * 16)];
+  }
+  return colour;
+}
 
 function divCreation(columns, rows) {
   for (let i = 0; i < rows * columns; i++) {
     const divs = document.createElement("div");
-    divs.classList.add("grid16");
+    divs.classList.add("grid");
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    divs.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = "black";
-      console.log(e.target);
+    divs.addEventListener("mouseover", () => {
+      divs.style.backgroundColor = randomColour();
     });
+    btnReset.addEventListener("click", () => {
+      divs.style.backgroundColor = "white";
+    });
+
     container.appendChild(divs);
   }
 }
 
 divCreation(32, 32);
+
+btn.addEventListener("click", () => {
+  const newGrid = prompt(
+    "Please type in your new grid size, number may not exceed 64."
+  );
+  if (newGrid <= 64) {
+    divCreation(newGrid, newGrid);
+  }
+});
