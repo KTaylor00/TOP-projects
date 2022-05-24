fe25e20a0c4ef0424078aca3060c9bf398f8fc12
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-24 13:43:32 +0200

    changed it to use class instead of factory functions

--- 04-library/script.js
+++ 04-library/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const author = document.querySelector("#author");
-const title = document.querySelector("#title");
-const pages = document.querySelector("#pages");
+const authorEl = document.querySelector("#author");
+const titleEl = document.querySelector("#title");
+const pagesEl = document.querySelector("#pages");
 const addBook = document.querySelector(".add-book");
 const createBook = document.querySelector(".create-book");
 const bookForm = document.querySelector(".book-form");
@@ -10,69 +10,66 @@ const displayBookContain = document.querySelector(".display-book-contain");
 
 let myLibrary = [];
 
-function Book(author, title, pages) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  // this.read = read;
-  // read = false;
-}
+class Book {
+  constructor(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  }
 
-function displayBook() {
-  const displayBook = document.querySelector("#display-book");
+  displayBook() {
+    const bookDisplay = document.querySelector("#display-book");
 
-  displayBook.textContent = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let author = document.createElement("p");
-    let title = document.createElement("p");
-    let pages = document.createElement("p");
-    let read = document.createElement("button");
-    let removeBtn = document.createElement("button");
+    bookDisplay.textContent = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      let card = document.createElement("div");
+      card.classList.add("card");
+      let displayAuthor = document.createElement("p");
+      let displayTitle = document.createElement("p");
+      let displayPages = document.createElement("p");
+      let read = document.createElement("button");
+      let removeBtn = document.createElement("button");
 
-    author.textContent = myLibrary[i].author;
-    title.textContent = myLibrary[i].title;
-    pages.textContent = myLibrary[i].pages;
+      displayAuthor.textContent = myLibrary[i].author;
+      displayTitle.textContent = myLibrary[i].title;
+      displayPages.textContent = myLibrary[i].pages;
 
-    removeBtn.innerText = "Remove";
-    removeBtn.addEventListener("click", remove);
-    removeBtn.id = `remove-${i}`;
+      removeBtn.innerText = "Remove";
+      removeBtn.addEventListener("click", this.remove.bind(this));
+      removeBtn.id = `remove-${i}`;
 
-    read.innerText = "Read";
-    read.classList.add("read");
+      read.innerText = "Read";
+      read.classList.add("read");
 
-    displayBook.appendChild(card);
-    card.appendChild(author);
-    card.appendChild(title);
-    card.appendChild(pages);
-    card.appendChild(read);
-    card.appendChild(removeBtn);
+      bookDisplay.appendChild(card);
+      card.appendChild(displayAuthor);
+      card.appendChild(displayTitle);
+      card.appendChild(displayPages);
+      card.appendChild(read);
+      card.appendChild(removeBtn);
+    }
+  }
+
+  remove(e) {
+    let position = e.target.id.split("-")[1];
+
+    myLibrary.splice(position, 1);
+    this.displayBook();
   }
 }
 
-function remove(e) {
-  let position = e.target.id.split("-")[1];
-
-  myLibrary.splice(position, 1);
-  displayBook();
-}
-
-function addBookToLibrary() {
-  let authorText = author.value;
-  let titleText = title.value;
-  let pagesText = pages.value;
-
-  const addedBook = new Book(authorText, titleText, pagesText);
-  myLibrary.push(addedBook);
-  console.log(myLibrary);
-  author.value = "";
-  title.value = "";
-  pages.value = "";
-  displayBook();
-}
 addBook.addEventListener("click", () => {
-  addBookToLibrary();
+  let authorText = authorEl.value;
+  let titleText = titleEl.value;
+  let pagesText = pagesEl.value;
+
+  let book = new Book(authorText, titleText, pagesText);
+  myLibrary.push(book);
+  console.log(myLibrary);
+  authorEl.value = "";
+  titleEl.value = "";
+  pagesEl.value = "";
+  book.displayBook();
   bookForm.classList.remove("show-book-form");
 });
 
